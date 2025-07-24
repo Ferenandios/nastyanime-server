41d339139ea9836595d8a8c2d0b6fadf9c75244a
Ferenandios <www.terunplay@gmail.com>
2025-07-24 19:21:27 +0300

    Idk what I just did rofl

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const fs = require("fs");
+const database = require("./messages.json");
 const express = require("express");
 const cors = require("cors");
 const { createServer } = require("http");
@@ -15,31 +17,54 @@ app.use(
   })
 );
 
+app.use(express.json());
+
 // Данные сообщений
-const messages = [
-  {
-    id: 0,
-    username: "Николай Князев",
-    text: "Добро пожаловать!",
-    date: "13/07/25 Вск 15:02:59",
-  },
-  {
-    id: 1,
-    username: "Николай Князев",
-    text: "Ку!",
-    date: "13/07/25 Вск 15:02:59",
-  },
-  {
-    id: 2,
-    username: "Николай Князев",
-    text: "Здарова тварына!",
-    date: "13/07/25 Вск 15:02:59",
-  },
-];
+const messages = JSON.parse(fs.readFileSync("./messages.json", "utf-8"));
 
 // Роут для получения сообщений
 app.get("/", (req, res) => {
   res.status(200).json(messages);
+});
+app.post("/", (req, res) => {
+  const message = req.body;
+  // Перезаписываем БД
+  function getFormattedDate() {
+    const now = new Date();
+
+    // Получаем день, месяц и год (двузначный)
+    const day = String(now.getDate()).padStart(2, "0");
+    const month = String(now.getMonth() + 1).padStart(2, "0");
+    const year = String(now.getFullYear()).slice(-2);
+
+    // Получаем день недели (сокращенный до 3 букв)
+    const daysOfWeek = ["Вск", "Пнд", "Втр", "Срд", "Чтв", "Птн", "Сбт"];
+    const dayOfWeek = daysOfWeek[now.getDay()];
+
+    // Получаем часы, минуты и секунды
+    const hours = String(now.getHours()).padStart(2, "0");
+    const minutes = String(now.getMinutes()).padStart(2, "0");
+    const seconds = String(now.getSeconds()).padStart(2, "0");
+
+    // Формируем строку
+    return `${day}/${month}/${year} ${dayOfWeek} ${hours}:${minutes}:${seconds}`;
+  }
+
+  const newMessage = {
+    ...message,
+    id: messages.length,
+    date: getFormattedDate(),
+  };
+
+  messages.push(newMessage);
+  fs.writeFile("./messages.json", JSON.stringify(messages), (err) => {
+    // Checking for errors
+    if (err) throw err;
+
+    // Success
+    console.log("Done writing");
+  });
+  res.send(newMessage);
 });
 
 // Создаем HTTP-сервер
@@ -62,15 +87,8 @@ io.on("connection", (socket) => {
 
   // Обработка новых сообщений
   socket.on("send-message", (message) => {
-    const newMessage = {
-      ...message,
-      id: messages.length,
-      date: new Date().toLocaleString("ru-RU"),
-    };
-
-    messages.push(newMessage);
-    io.emit("receive-message", newMessage); // Отправляем всем клиентам
-    console.log("Новое сообщение:", newMessage);
+    io.emit("receive-message", message); // Отправляем всем клиентам
+    console.log("Новое сообщение:", message);
   });
 
   socket.on("disconnect", () => {
